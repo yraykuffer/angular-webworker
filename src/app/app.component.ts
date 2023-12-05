964ac735f0ee43b8c37de9047140306d573b9781
Fernando Yray <fernandojsyray@gmail.com>
2023-12-06 06:02:57 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeCalculator } from './app.prime';
 
 @Component({
   selector: 'app-root',
@@ -6,17 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-webworker';
+  title = 'Web worker sample';
+   prime10 : number = 0;
+   prime10000 : number = 0;
+
+   find10thPrimeNumber() {
+      this.prime10 = PrimeCalculator.findNthPrimeNumber(10);
+   }
+
+   worker = new Worker(new URL('./app.worker', import.meta.url));
+
+   find10000thPrimeNumber() {
+    this.prime10000 = 0;
+      if (typeof Worker !== 'undefined') {
+         // Create a new
+
+         this.worker.onmessage = ({ data }) => {
+         this.prime10000 = data;
+         };
+         this.worker.postMessage(10000);
+      } else {
+         // Web Workers are not supported in this environment.
+         // You should add a fallback so that your program still executes correctly.
+      }
+   }
+
+   async find10000thPrimeNumberAsync() {
+    this.prime10000 = await PrimeCalculator.findNthPrimeNumberAsync(10000);
+
+   }
 }
 
-if (typeof Worker !== 'undefined') {
-  // Create a new
-  const worker = new Worker(new URL('./app.worker', import.meta.url));
-  worker.onmessage = ({ data }) => {
-    console.log(`page got message: ${data}`);
-  };
-  worker.postMessage('hello');
-} else {
-  // Web Workers are not supported in this environment.
-  // You should add a fallback so that your program still executes correctly.
-}
--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -1,6 +1,9 @@
 /// <reference lib="webworker" />
 
+import { PrimeCalculator } from "./app.prime";
+
 addEventListener('message', ({ data }) => {
-  const response = `worker response to ${data}`;
+  // const response = `worker response to ${data}`;
+  const response = PrimeCalculator.findNthPrimeNumber(parseInt(data));
   postMessage(response);
 });
